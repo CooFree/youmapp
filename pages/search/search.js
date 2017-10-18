4d218f752edce1ec14288fb9ec7803d6289c2a1c
joek2222 <497925865@qq.com>
2017-10-18 14:14:10 +0800

    df

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,6 +1,7 @@
 // pages/search/search.js
-//import utility from '../../utils/utility';
-//const homeChannel = new HomeChannel();
+import util from '../../utils/util';
+import HomeChannel from '../../channels/home';
+const homeChannel = new HomeChannel();
 
 Page({
 
@@ -15,29 +16,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var _this = this;
-    wx.request({
-      url: 'https://app.you.camel.com.cn/search.aspx',
-      data: {},
-      method: 'get',
-      header: { 'Content-Type': 'application/json' },
-      success: function (res) {
-        res.data.list = res.data.list.map(function (item) {
-          item = decodeURI(item);
-          return item;
-        })
-
-        _this.setData({ searchKeywordList: res.data.list });
-      },
-      fail: function () {
-
-      }
-    })
-
-    // homeChannel.getSearchKeyword().then(data => {
-    //   console.log(data);
-    //   this.setData({ searchKeywordList: data });
-    // })
+    homeChannel.getSearchKeyword().then(data => {
+      this.setData({ searchKeywordList: data });
+    });
   },
 
   /**

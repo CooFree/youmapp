d4c4b10a933b5c90cac1ce9eb46b157c9b10cad6
leeggco <leeggco@users.noreply.github.com>
2017-10-25 10:41:27 +0800

    1

--- pages/product/productDetail/productDetail.js
+++ pages/product/productDetail/productDetail.js
@@ -14,6 +14,7 @@ Page({
             duration: 500,
             circular: true
         },
+        previewCommentpic: '',
         selectColorIndex: -1,
         selectSizeIndex: -1,
         showSelector: false,
@@ -179,7 +180,7 @@ Page({
     },
     setStore: function (event) {
         let prodId = event.currentTarget.dataset.prodId;
-        let {storeflag} = this.data;
+        let { storeflag } = this.data;
         if (storeflag === 1) {
             productChannel.deleteProductStore(prodId).then(data => {
                 this.setData({

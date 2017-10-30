c340c30cda1d607a38b0b96b3b077a933e62daa6
leeggco <leeggco@users.noreply.github.com>
2017-10-30 11:30:02 +0800

    12

--- pages/portal/help/help.js
+++ pages/portal/help/help.js
@@ -1,66 +1,78 @@
 // pages/portal/help/help.js
+import PortalChannel from '../../../channels/portal';
+import util from '../../../utils/util';
+const portalChannel = new PortalChannel();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let code = options.code;
+    portalChannel.getHelpData(code).then(data => {
+      this.setData({
+        helpData: data
+      })
+
+      wx.setNavigationBarTitle({
+        title: util.decodeURI(data.title)
+      })
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
